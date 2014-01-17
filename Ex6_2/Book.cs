6c252f8247648e0f21fdcd7d3f28ce265c5b6041
Jack Morris <jack.morris10@gmail.com>
2014-01-17 22:14:10 +0000

    Added Exercises from Chapter 6 (2)

---
+++ Ex6_2/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex6_2
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int isbn;
+
+        public void read()
+        {
+            // Provide some mechanism to 'read' the book.
+        }
+
+        public void shelve()
+        {
+            // Provide some mechanism to 'shelve' the book.
+        }
+    }
+}
