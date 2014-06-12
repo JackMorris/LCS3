f8c126d9f2c33a0df9a4fb0f3f1082bf68892b30
Jack Morris <jack.morris10@gmail.com>
2014-06-12 18:03:21 +0100

    Updated Ex5_2 for correct program flow.
    'inputInt' is now tested to see if it's a multiple of 10 before even and
    odd, as multiple of 10 => even. Therefore, only one message is printed for
    each input.

--- Ex5_2/Program.cs
+++ Ex5_2/Program.cs
@@ -4,31 +4,30 @@ namespace Ex5_2
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             // Get integer input.
             Console.Write("Enter value: ");
-            var input = Convert.ToInt32(Console.ReadLine());
+            var inputInt = Convert.ToInt32(Console.ReadLine());
 
-            // Three cases: ==0, 0<input<=100 (test for even, odd, multiple of ten also), >100 (too large).
-            if (input == 0)
+            // Three cases: == 0, 0 < input <= 100 (test for even, odd, multiple of ten also), > 100 (too large).
+            if (inputInt == 0)
             {
                 Console.WriteLine("Input is 0");
             }
-            else if (input <= 100)
+            else if (inputInt <= 100)
             {
-                if (input % 2 == 0)
+                if (inputInt % 10 == 0)
+                {
+                    Console.WriteLine("Input is multiple of 10");
+                }
+                else if (inputInt % 2 == 0)
                 {
                     Console.WriteLine("Input is even");
                 }
                 else
                 {
                     Console.WriteLine("Input is odd");
-                }
-
-                if (input % 10 == 0)
-                {
-                    Console.WriteLine("Input is multiple of 10");
                 }
             }
             else
