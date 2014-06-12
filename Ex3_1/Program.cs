7c9845b880600709ee6ce71abdfa22e3f5fb244a
Jack Morris <jack.morris10@gmail.com>
2014-06-12 17:40:32 +0100

    Updated Ex3_1 and Ex3_2 for better readability

--- Ex3_1/Program.cs
+++ Ex3_1/Program.cs
@@ -12,8 +12,11 @@ namespace Ex3_1
             var myChar = 'Z';
             var myString = "The quick brown fox jumped over the lazy dogs.";
 
-            Console.WriteLine("myInt: {0}\nmyFloat: {1}\nmyDouble: {2}\nmyChar: {3}\nmyString: {4}", 
-                myInt, myFloat, myDouble, myChar, myString);
+            Console.WriteLine("myInt:\t\t{0}", myInt);
+            Console.WriteLine("myFloat:\t{0}", myFloat);
+            Console.WriteLine("myDouble:\t{0}", myDouble);
+            Console.WriteLine("myChar:\t\t{0}", myChar);
+            Console.WriteLine("myString:\t{0}", myString);
         }
     }
 }
--- Ex3_2/Program.cs
+++ Ex3_2/Program.cs
@@ -12,8 +12,11 @@ namespace Ex3_2
             var myChar = 'Z';
             var myString = "The quick brown fox jumped over the lazy dogs.";
 
-            Console.WriteLine("myInt: {0}\nmyFloat: {1}\nmyDouble: {2}\nmyChar: {3}\nmyString: {4}\n",
-                myInt, myFloat, myDouble, myChar, myString);
+            Console.WriteLine("myInt:\t\t{0}", myInt);
+            Console.WriteLine("myFloat:\t{0}", myFloat);
+            Console.WriteLine("myDouble:\t{0}", myDouble);
+            Console.WriteLine("myChar:\t\t{0}", myChar);
+            Console.WriteLine("myString:\t{0}", myString);
 
             myInt = 25;
             myFloat = 100.3f;
@@ -21,8 +24,11 @@ namespace Ex3_2
             myChar = 'M';
             myString = "A quick movement of the enemy will jeopardize six gun boats.";
 
-            Console.WriteLine("myInt: {0}\nmyFloat: {1}\nmyDouble: {2}\nmyChar: {3}\nmyString: {4}\n",
-                myInt, myFloat, myDouble, myChar, myString);
+            Console.WriteLine("myInt:\t\t{0}", myInt);
+            Console.WriteLine("myFloat:\t{0}", myFloat);
+            Console.WriteLine("myDouble:\t{0}", myDouble);
+            Console.WriteLine("myChar:\t\t{0}", myChar);
+            Console.WriteLine("myString:\t{0}", myString);
         }
     }
 }
